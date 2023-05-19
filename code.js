32275b717511bd54e46b15245d660d7ad7e4609c
Dario Cruz <rio2012@gmail.com>
2023-05-19 13:04:58 -0400

    Add class for node items.

--- code.js
+++ code.js
@@ -1,5 +1,16 @@
 class linkedList {
     constructor() {
-        
+
     }
 }
+
+
+// Create a node class that contains a param for a value and the
+// next node in the link. 
+class nodeItem {
+    constructor(value = null, nextNode = null) {
+        this.value = value
+        this.nextNode = nextNode
+    }
+}
+
